2b98ebbe4aa0a796ef7e1492b44f34e81a413caa
Yax <ypjuarez11@gmail.com>
2023-03-29 09:11:06 -0400

    Navigation bar and routes added

--- src/NestedNavBar.js
+++ src/NestedNavBar.js
@@ -1,42 +1,30 @@
 import React from 'react'
-import { useNavigate } from 'react-router-dom'
+
 import { NavLink } from "react-router-dom"
 import { Button} from "@mantine/core"
 
 
 function NestedNavBar({close}) {
     
-  let navigate = useNavigate()
-  const handleLogOut = () => {
-    fetch("/logout", {
-      method: "DELETE",
-    }).then((res)=> {
-      if (res.ok) {
-        navigate("/");
-      }
-    })
-
-  }
+ 
 
   return (
     <div className="shelf-container">
      <header className="header">
-     <div className="close-movie-form" onClick={() => close()}> X </div>
+     <div className="close-navbar" onClick={() => close()}> X </div>
      </header>
-     <div className="shelf">
+     <div className="nav-shelf">
      <NavLink to="/home">
         <Button className='nav-button'>
            Home
          </Button>
        </NavLink>
-       <NavLink to="/reviews" >
+       <NavLink to="/about" >
         <Button className='nav-button'> 
-          Reviews
+          About
         </Button>
        </NavLink>
-               <Button className='nav-button' onClick={handleLogOut} >
-                 Log Out
-                 </Button>
+              
      </div>            
     
     </div>
