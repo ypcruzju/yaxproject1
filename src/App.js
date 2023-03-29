34571ad2b7b4a259ca9852accae34817ddae545c
Yax <ypjuarez11@gmail.com>
2023-03-29 10:41:00 -0400

    Video containers and slider

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import NavBar from './NavBar';
 import './App.css';
+import Home from './Home';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <NavBar />
+        <Home/>
       </header>
     </div>
   );
---
+++ src/VideoCard.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+
+
+
+function VideoCard({video}) {
+    const snip = video.snippet
+    const vid = video.snippet.resourceId.videoId
+    const thumbnail = video.snippet.thumbnails.standard.url
+    const vidSrc = `https://www.youtube.com/embed/${vid}`
+
+    // console.log(video.id)
+
+return   (
+    <div className="video-container">
+        <div>
+          {/* <img  alt={snip.title} onClick={playVideo} /> */}
+      <h4 className='video-name'>{snip.title}</h4>
+      </div>
+      <iframe className="staffPick-img"  src={vidSrc} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+    <div></div>
+    </div>
+
+)
+
+}
+
+export default VideoCard
